d6578e2a8ce4f26d0f6c04211ddc22233ce7eeba
Patrik Oliveira <patrik.oliveira@rarolabs.com.br>
2020-04-16 09:54:44 -0300

    fix: Adicionando o LoggerMiddleware como default

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 // import { LoggerMiddleware } from "./src/middlewares/loggerMiddleware";
 // export * from "./src/middlewares/loggerMiddleware";
 export { LoggerMiddleware } from "./src/middlewares/loggerMiddleware";
+export { LoggerService } from "./src/services/loggerService";
